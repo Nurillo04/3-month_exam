72db67396a886d5721b88653244e5d3265535f15
Nurillo04 <aminjonovnurillo95@gmail.com>
2023-01-19 13:49:10 +0500

    kabisa yili

--- masala.js
+++ masala.js
@@ -2,11 +2,36 @@
 //
 
 
+
+
 // let year = Number(prompt("kerakli yilni kiritng"));
 
 // let xisob = year % 4;
 // xisob == 0 && console.log(year, ' yil kabisa yili');
 // xisob !== 0 && console.log(year, ' yil kabisa yili emas');
+
+
+
+
+//  kabisa yilini hissoblovchi dasturini tuzish
+
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return `${year}- Kabisa yili`
+    } else {
+        return `${year}- Kabisa yili emas`
+    }
+
+}
+[2000, 2004, 4, 2400, 2404, 1300, 900, 1300, 1200, 2000, 1800, 1900, 2100, 1, 4000, 5000].forEach((year) =>
+    console.log(isLeapYear(year))
+);
+
+
+
+
+
+
 
 
 
